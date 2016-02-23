6d5b363f2c0cebfbaed773d9a865e031083f203d
RobertSuaus <roberto_ivan@outlook.es>
2016-02-23 00:11:40 -0600

    Added files via upload

---
+++ NQueens/NQueens/Classes/Util.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NQueens.Classes
+{
+    class Util
+    {
+        //Muta un bit de una solucion, si se cumple Pm
+        public static String Mutate(String solution, float Pm)
+        {
+            //Comprobar que la probabilidad sea menor que Pm
+            if (false)
+            {
+                return solution;
+            }
+            Random random = new Random();
+            int i = random.Next(0, 24);
+            
+            StringBuilder mutatedSolution = new StringBuilder(solution);
+            if (mutatedSolution[i] == '0')
+            {
+                mutatedSolution[i] = '1';
+            }
+            else
+            {
+                mutatedSolution[i] = '0';
+            }
+
+            solution = mutatedSolution.ToString();
+            return solution;
+        }
+
+        
+    }
+}
---
+++ NQueens/NQueens/Program.cs
@@ -0,0 +1,27 @@
+﻿using NQueens.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NQueens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Almacenar numeros en binario en un string
+            String solucion="";
+            int[] n = { 5 , 4, 2, 6, 5, 2, 1, 2};
+            foreach (int number in n)
+            {
+                solucion += Convert.ToString(number, 2);
+            }
+            Console.WriteLine( solucion);
+            int output = Convert.ToInt32(solucion, 2);
+            Console.WriteLine(output);
+            Console.Read();
+        }
+    }
+}
